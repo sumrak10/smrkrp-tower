b78ff48aaf37ca03dd7a7d0d49d6d9ec44c83b58
sumrak10 <nurlan.urazkulov@gmail.com>
2022-05-29 13:49:05 +0500

    first

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 var tg = window.Telegram.WebApp;
-
+tg.expand()
 var
     cvs     = document.getElementById('canvas'),
     ctx     = cvs.getContext('2d'),
     wWidth  = window.innerWidth,
     wHeight = window.innerHeight;
 
-tg.expand()
 cvs.width = wWidth;
 cvs.height = wHeight;
+
+var isRight = true;
+var i = 1;
+function gameLoop() {
+    clearCanvas();
+    drawRect(i,100,100,100,'white');
+    if (i == wWidth-100) {
+        isRight = false;
+    } 
+    if (i == 0){
+        isRight = true;
+    }
+    if (isRight) {
+        i++;
+    } else {
+        i--;
+    }
+    // window.requestAnimationFrame(draw);
+}
+window.onload = function() {
+    setInterval(gameLoop, 100/60); //10 fpc
+    // window.requestAnimationFrame(draw);
+};
+
 
 function mapInt(x, in_min, in_max, out_min, out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -20,14 +43,9 @@ function clearCanvas() {
     ctx.fillRect(0, 0, wWidth, wHeight );
     ctx.closePath();
 }
-function loop() {
-    clearCanvas();
-    // window.requestAnimationFrame(draw);
+function drawRect(x1,y1,x2,y2,color) {
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.fillRect(x1,y1,x2,y2);
+    ctx.closePath();
 }
-
-window.onload = function() {
-    console.log(tg.viewportHeight)
-    tg.MainButton.hide();
-    setInterval(loop, 100/60); //10 fpc
-    // window.requestAnimationFrame(draw);
-};
