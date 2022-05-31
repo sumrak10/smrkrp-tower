85ab1a11d30ce5ffc4531c358dd133a2740e22fe
sumrak10 <nurlan.urazkulov@gmail.com>
2022-06-01 02:57:55 +0500

    tg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var wtg = window.Telegram.WebApp,
+var tg = window.Telegram.WebApp,
     tg  = Telegram.WebApp;
-wtg.expand();
+tg.expand();
 var
     cvs     = document.getElementById('canvas'),
     ctx     = cvs.getContext('2d'),
@@ -22,13 +22,13 @@ var game = false,
 // const bg_color = '#122459'
 const colors = ['#048ABF','#04B2D9','#05DBF2','#05F2F2']
 const bg_color = [10,10,100]
-if (wtg.ready()) {
-    colors = [wtg.ThemeParams.bg_color,
-            wtg.ThemeParams.text_color,
-            wtg.ThemeParams.hint_color,
-            wtg.ThemeParams.link_color,
-            wtg.ThemeParams.button_color,
-            wtg.ThemeParams.button_text_colorString]
+if (tg.ready()) {
+    colors = [tg.ThemeParams.bg_color,
+            tg.ThemeParams.text_color,
+            tg.ThemeParams.hint_color,
+            tg.ThemeParams.link_color,
+            tg.ThemeParams.button_color,
+            tg.ThemeParams.button_text_colorString]
 }
 let isRight = true;
 
@@ -131,12 +131,12 @@ function gameLoop() {
         ctx.font = "50px RobotoRegular";
         ctx.textAlign = 'center'
         // ctx.fillText('Счет: '+(level-1), wWidth/2,wHeight/2-50);
-        if (wtg.ready()) {
+        if (tg.ready()) {
             drawStartButton(wWidth/2-100,wHeight/2,200,30,'#048ABF', 'Рестарт');
-            wtg.MainButton.text = 'Готово';
-            wtg.MainButton.color = colors[0];
-            wtg.MainButton.textColor = '#ffffff';
-            wtg.MainButton.show();
+            tg.MainButton.text = 'Готово';
+            tg.MainButton.color = colors[0];
+            tg.MainButton.textColor = '#ffffff';
+            tg.MainButton.show();
             ctx.fillText('гл.кнопка '+(level-1), wWidth/2,wHeight/2-50);
         }
     }
@@ -156,8 +156,8 @@ function gameLoop() {
 window.onload = function() {
     console.log('v1.0');
     cvs.addEventListener("touchstart", handleStart, false);
-    tg.onEvent('mainButtonClicked', function(){
-        wtg.sendData("level: " + level);
+    Telegram.WebApp.onEvent('mainButtonClicked', function(){
+        tg.sendData("level: " + level);
     });
     
     window.requestAnimationFrame(gameLoop);
